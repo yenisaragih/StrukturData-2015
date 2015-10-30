b437da43eacad5b9fca274e9414a65ed4b573f6f
yenisaragih <yeni.hanafiah@gmail.com>
2015-10-30 16:47:07 +0700

    Vector3D

---
+++ 01-Vector/Vector3D.java
@@ -0,0 +1,25 @@
+
+/**
+ * Write a description of class Vector3D here.
+ * 
+ * @author (Yeni H Saragih) 
+ */
+public class Vector3D
+{
+    // instance variables - replace the example below with your own
+     public double x;
+     public double y;
+     public double z;
+    /**
+     * Constructor for objects of class Vector3D
+     */
+    public Vector3D(double x,double y,double z)
+    {
+        // initialise instance variables
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+        }
+
